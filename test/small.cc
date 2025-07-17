6771629c217960b343ae561a91df7b43759e8081
hz <hedzrz@gmail.com>
2025-07-17 22:52:35 +0800

    add ctest targets

---
+++ test/small.cc
@@ -0,0 +1,20 @@
+/*
+ * @copy Copyright © 2016 - 2024 Hedzr Yeh.
+ *
+ * trie - C++17/C++20 Text Difference Utilities Library
+ *
+ * This file is part of trie.
+ *
+ * trie is free software: you can redistribute it and/or modify
+ * it under the terms of the Apache 2.0 License.
+ * Read /LICENSE for more information.
+ */
+
+#include <iomanip>
+#include <iostream>
+int main() {
+	std::cout << "Hello, World! Y1=" << Y1 << '\n';
+#if defined(Y2)
+	std::cout << "Y2 defined." << '\n';
+#endif
+}
