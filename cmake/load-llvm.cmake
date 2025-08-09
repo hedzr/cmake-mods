d11abd4402925eef741f1b4312ad0a3a853779ca
hz <hedzrz@gmail.com>
2025-08-09 13:41:53 +0800

    fix load-llvm (eventhough it's never been used)

--- cmake/load-llvm.cmake
+++ cmake/load-llvm.cmake
@@ -1,36 +1,39 @@
+# DON'T USE THIS FILE PLEASE, it's not a good approach to using LLVM.
+# Uses llvm-helpers.macro to integrate your target with LLVM support.
+
 
 # set LLVM base directory
 #     https://llvm.org/docs/CMake.html
-IF (${MAC})
+if(${MAC})
     if(EXISTS "${LLVM_ROOT}/bin/clang")
     else()
         set(LLVM_ROOT /opt/homebrew/opt/llvm)
         if(EXISTS "${LLVM_ROOT}/bin/clang")
         else()
             set(LLVM_ROOT /usr/local/opt/llvm)
-        endif
+        endif()
         if(EXISTS "${LLVM_ROOT}/bin/clang")
             set(LLVM_DIR "${LLVM_ROOT}/share/cmake/modules")
         endif()
     endif()
-ELSEIF (${UNIX})
+elseif(${UNIX})
     if(EXISTS "${LLVM_ROOT}/bin/clang")
     else()
-    find_program(LLVM_CLANGXX clang++)
+        find_program(LLVM_CLANGXX clang++)
         if(LLVM_CLANGXX_FOUND)
         endif()
     endif()
-ELSE ()
+else()
     #set(LLVM_CMAKE_DIR "/usr/share/llvm-3.6/share/llvm/cmake")
     #set(LLVM_CMAKE_DIR "${LLVM_DIR}/share/llvm/cmake")
-ENDIF ()
+endif()
 
 if(EXISTS "${LLVM_ROOT}/bin/clang")
 else()
-	message(FATAL "CANNOT FOUND LLVM, TRY SPECIFY LLVM_ROOT MANUALLY.")
+    message(FATAL "CANNOT FOUND LLVM, TRY SPECIFY LLVM_ROOT MANUALLY.")
 endif()
 set(LLVM_CMAKE_DIR "${LLVM_DIR}")
-MESSAGE("LLVM_CMAKE_DIR = ${LLVM_CMAKE_DIR}")
+message("LLVM_CMAKE_DIR = ${LLVM_CMAKE_DIR}")
 
 
 set(LLVM_TARGETS_TO_BUILD X86)
@@ -42,14 +45,14 @@ message(STATUS "Found LLVM ${LLVM_PACKAGE_VERSION}")
 message(STATUS "Using LLVMConfig.cmake in: ${LLVM_DIR}")
 
 
-SET(CMAKE_CXX_COMPILER_ENV_VAR "clang++")
-SET(CMAKE_CXX_FLAGS "-std=c++11")
-SET(CMAKE_CXX_FLAGS_DEBUG "-g")
-SET(CMAKE_CXX_FLAGS_MINSIZEREL "-Os -DNDEBUG")
-SET(CMAKE_CXX_FLAGS_RELEASE "-O4 -DNDEBUG")
-SET(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O2 -g")
+set(CMAKE_CXX_COMPILER_ENV_VAR "clang++")
+set(CMAKE_CXX_FLAGS "-std=c++11")
+set(CMAKE_CXX_FLAGS_DEBUG "-g")
+set(CMAKE_CXX_FLAGS_MINSIZEREL "-Os -DNDEBUG")
+set(CMAKE_CXX_FLAGS_RELEASE "-O4 -DNDEBUG")
+set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O2 -g")
 
-#SET(EXECUTABLE_OUTPUT_PATH ${PROJECT_SOURCE_DIR}/bin)
+#set(EXECUTABLE_OUTPUT_PATH ${PROJECT_SOURCE_DIR}/bin)
 
 include_directories(${LLVM_INCLUDE_DIRS})
 add_definitions(${LLVM_DEFINITIONS})
